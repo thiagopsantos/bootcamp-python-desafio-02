986d73f05e807cc8296ea6d8bccfd3a685eb67c9
Thiago Santos <thiagop.santos182@gmail.com>
2024-04-16 09:45:42 -0400

    Implementing login

--- app.py
+++ app.py
@@ -9,7 +9,24 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin123@127.0.0.1:3306/flask-meals'
 
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
+
+login_manager.login_view = 'login'
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+
+    if username and password:
+        user = User.query.filter_by(username=username).first()
+        if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
+            login_user(user)
+            return jsonify({"message": "Autenticação realizada com sucesso"})
+    return jsonify({"message": "Credenciais inválidas"}), 400
 
 @app.route('/users', methods=['POST'])
 def create_user():
